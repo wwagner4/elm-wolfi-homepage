e11746915572e0198a0dd273d63184a7f9f9b7f2
wwagner4 <wwagner4@gmail.com>
2016-01-08 22:47:27 +0100

    some renaming

--- Index.elm
+++ Index.elm
@@ -21,34 +21,36 @@ ww = {
   imageUrl = "ww.jpg"}
 
 
-txtElem : String -> Int -> Element
-txtElem str h = str
+txt : String -> Int -> Element
+txt str h = str
   |> fromString
   |> typeface typefaces
   |> Text.height (toFloat h)
   |> centered
 
-txtCont : String -> Int -> Element
-txtCont str h = container contWidth (h + 10) middle (txtElem str h)
+txtCentered : String -> Int -> Element
+txtCentered str h = container contWidth (h + 10) middle (txt str h)
+
 elemCont elem = container contWidth (heightOf elem)  middle elem
 
-txtFlow : Elem -> Element
-txtFlow elem = flow down [
-  txtCont elem.title txtHeight1,
-  txtCont elem.subTitle txtHeight2]
+txtElem : Elem -> Element
+txtElem elem = flow down [
+  txtCentered elem.title txtHeight1,
+  txtCentered elem.subTitle txtHeight2]
 
 
-txtCont1 : Elem -> Element
-txtCont1 elem = container contWidth contHeight middle (txtFlow elem)
+txtElemCentered : Elem -> Element
+txtElemCentered elem = container contWidth contHeight middle (txtElem elem)
 
 allFlow : Elem -> Element
 allFlow elem = flow inward [
-  txtCont1 elem,
+  txtElemCentered elem,
   elemCont (opacity 0.5 (fittedImage contWidth contHeight elem.imageUrl))]
 
-allForm = allFlow ww
+allForm : Float -> Float -> Form
+allForm x y = allFlow ww
   |> toForm
-  |> move (0, 0)
+  |> move (x, y)
 
 main : Element
-main = collage 700 700 [allForm]
+main = collage 700 700 [allForm 0 0]
