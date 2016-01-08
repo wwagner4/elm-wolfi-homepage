730043730c5d602d8d1633e875429f58b1c382cf
wwagner4 <wwagner4@gmail.com>
2016-01-08 20:11:31 +0100

    two text heights

--- Index.elm
+++ Index.elm
@@ -1,20 +1,28 @@
 import Graphics.Element exposing (..)
 import Text exposing (..)
 
-txtHeight = 100
-contWidth = 400
-contHeight = 300
+txtHeight1 = 70
+txtHeight2 = 30
+contWidth = 200
+contHeight = 150
+typefaces = ["trattatello","arial","sans-serif"]
 
-txtElem : String -> Element
-txtElem str = str |> fromString |> Text.height txtHeight |> centered
 
-txtCont str = container contWidth (txtHeight + 5) middle (txtElem str)
+txtElem : String -> Int -> Element
+txtElem str h = str
+  |> fromString
+  |> typeface typefaces
+  |> Text.height (toFloat h)
+  |> centered
+
+txtCont : String -> Int -> Element
+txtCont str h = container contWidth (h + 5) middle (txtElem str h)
 elemCont elem = container contWidth (heightOf elem)  middle elem
 
 
 txtFlow = flow down [
-  txtCont "Hallo",
-  txtCont "Wolfi"]
+  txtCont "Hallo" txtHeight1,
+  txtCont "Wolfi" txtHeight2]
 
 
 txtCont1 = container contWidth contHeight middle txtFlow
