67a2d34263387182c040dd661b3c0fe736277666
wwagner4 <wwagner4@gmail.com>
2016-01-08 17:36:25 +0100

    format center + show image

--- Index.elm
+++ Index.elm
@@ -1,10 +1,21 @@
 import Graphics.Element exposing (..)
 import Text exposing (..)
 
-txtElem : String -> Element
-txtElem str = str |> fromString |> centered
+txtHeight = 30
 
+txtElem : String -> Element
+txtElem str = str |> fromString |> Text.height txtHeight |> centered
+
+txtCont str = container 300 (txtHeight + 5) middle (txtElem str)
+elemCont elem = container 300 (heightOf elem)  middle elem
+
+
+t : Element
+t = flow down [
+  txtCont "Hallo",
+  txtCont "Wolfi",
+  elemCont (fittedImage 200 150 "ww.jpg")]
 
 
 main : Element
-main = txtElem "Hallo Wolfi 1"
+main = t
