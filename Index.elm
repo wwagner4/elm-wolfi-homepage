a9ac71d0df35fc3458b0da1456943d212d0d5e2a
wwagner4 <wwagner4@gmail.com>
2016-01-08 21:59:59 +0100

    added Elem alias

--- Index.elm
+++ Index.elm
@@ -5,8 +5,20 @@ import Text exposing (..)
 txtHeight1 = 35
 txtHeight2 = 15
 contWidth = 350
-contHeight = 300
+contHeight = 250
 typefaces = ["sans-serif"]
+
+type alias Elem =
+  { title : String,
+    subTitle : String,
+    imageUrl : String
+  }
+
+ww : Elem
+ww = {
+  title = "Wolfgang Wagner",
+  subTitle = "yet another programmer",
+  imageUrl = "ww.jpg"}
 
 
 txtElem : String -> Int -> Element
@@ -20,20 +32,23 @@ txtCont : String -> Int -> Element
 txtCont str h = container contWidth (h + 10) middle (txtElem str h)
 elemCont elem = container contWidth (heightOf elem)  middle elem
 
-
-txtFlow = flow down [
-  txtCont "Wolfgang Wagner" txtHeight1,
-  txtCont "font of programming" txtHeight2]
-
-
-txtCont1 = container contWidth contHeight middle txtFlow
-
-allFlow = flow inward [
-  txtCont1,
-  elemCont (opacity 0.5 (fittedImage contWidth contHeight "ww.jpg"))]
+txtFlow : Elem -> Element
+txtFlow elem = flow down [
+  txtCont elem.title txtHeight1,
+  txtCont elem.subTitle txtHeight2]
 
 
-allForm = allFlow |> toForm |> move (0, 0)
+txtCont1 : Elem -> Element
+txtCont1 elem = container contWidth contHeight middle (txtFlow elem)
+
+allFlow : Elem -> Element
+allFlow elem = flow inward [
+  txtCont1 elem,
+  elemCont (opacity 0.5 (fittedImage contWidth contHeight elem.imageUrl))]
+
+allForm = allFlow ww
+  |> toForm
+  |> move (0, 0)
 
 main : Element
 main = collage 700 700 [allForm]
