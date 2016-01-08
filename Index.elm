53be2ec60d7fba1d2848386e44efbaf2645e350b
wwagner4 <wwagner4@gmail.com>
2016-01-08 22:58:16 +0100

    renaming again

--- Index.elm
+++ Index.elm
@@ -5,7 +5,7 @@ import Text exposing (..)
 txtHeight1 = 35
 txtHeight2 = 15
 contWidth = 350
-contHeight = 250
+contHeight = 350
 typefaces = ["sans-serif"]
 
 type alias Elem =
@@ -31,8 +31,6 @@ txt str h = str
 txtCentered : String -> Int -> Element
 txtCentered str h = container contWidth (h + 10) middle (txt str h)
 
-elemCont elem = container contWidth (heightOf elem)  middle elem
-
 txtElem : Elem -> Element
 txtElem elem = flow down [
   txtCentered elem.title txtHeight1,
@@ -42,15 +40,18 @@ txtElem elem = flow down [
 txtElemCentered : Elem -> Element
 txtElemCentered elem = container contWidth contHeight middle (txtElem elem)
 
-allFlow : Elem -> Element
-allFlow elem = flow inward [
+elemCont elem = container contWidth (heightOf elem)  middle elem
+
+elemElem : Elem -> Element
+elemElem elem = flow inward [
   txtElemCentered elem,
   elemCont (opacity 0.5 (fittedImage contWidth contHeight elem.imageUrl))]
 
-allForm : Float -> Float -> Form
-allForm x y = allFlow ww
+elemForm : Elem -> Float -> Float -> Form
+elemForm elem x y = elemElem elem
   |> toForm
   |> move (x, y)
 
 main : Element
-main = collage 700 700 [allForm 0 0]
+main = collage 700 700 [
+  elemForm ww 0 0]
