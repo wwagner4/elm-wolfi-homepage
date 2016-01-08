6102580897ccb9b48286446918cb5109a659662a
wwagner4 <wwagner4@gmail.com>
2016-01-08 19:55:48 +0100

    display of container object

--- Index.elm
+++ Index.elm
@@ -1,22 +1,27 @@
 import Graphics.Element exposing (..)
 import Text exposing (..)
 
-txtHeight = 30
+txtHeight = 100
+contWidth = 400
+contHeight = 300
 
 txtElem : String -> Element
 txtElem str = str |> fromString |> Text.height txtHeight |> centered
 
-txtCont str = container 300 (txtHeight + 5) middle (txtElem str)
-elemCont elem = container 300 (heightOf elem)  middle elem
+txtCont str = container contWidth (txtHeight + 5) middle (txtElem str)
+elemCont elem = container contWidth (heightOf elem)  middle elem
 
 
 txtFlow = flow down [
   txtCont "Hallo",
   txtCont "Wolfi"]
 
+
+txtCont1 = container contWidth contHeight middle txtFlow
+
 allFlow = flow inward [
-  txtFlow,
-  elemCont (opacity 0.5 (fittedImage 200 150 "ww.jpg"))]
+  txtCont1,
+  elemCont (opacity 0.5 (fittedImage contWidth contHeight "ww.jpg"))]
 
 
 
