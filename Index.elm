fe411ccf7cca490248ed3d4d6f312038bcd1dbf8
wwagner4 <wwagner4@gmail.com>
2016-01-13 06:49:00 +0100

    mousclick is working

--- Index.elm
+++ Index.elm
@@ -3,6 +3,7 @@ import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Window exposing (..)
 import Time exposing (..)
+import Mouse exposing (..)
 
 
 ww : Elem
@@ -21,11 +22,15 @@ pos : Time -> Float
 pos t = 500 * (sin (t * 0.001))
 
 
-elemPlane : (Int, Int) -> Time -> Element
-elemPlane (x, y) t = collage x y [
-  elemForm cn (pos t) 0,
+elemPlane : (Int, Int) -> Time -> Int -> Element
+elemPlane (x, y) t c = collage x y [
+  elemForm cn (pos t) ((toFloat c) * 100),
   elemForm ww 0 (pos t)]
 
+countClick : Signal Int
+countClick =
+  Signal.foldp (\clk count -> count + 1) 0 Mouse.clicks
+
 main : Signal Element
-main = Signal.map2 elemPlane Window.dimensions (Time.every (Time.second * 0.01))
+main = Signal.map3 elemPlane Window.dimensions (Time.every (Time.second * 0.01)) countClick
 
