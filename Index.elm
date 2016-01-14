dc725d2a8fa9d2cc0b44b92565d1bcf0304d82fd
wwagner4 <wwagner4@gmail.com>
2016-01-14 08:01:55 +0100

    update view first time compellable

--- Index.elm
+++ Index.elm
@@ -8,6 +8,8 @@ import Mouse exposing (..)
 
 type alias MousePos = (Int, Int)
 
+type alias ScreenSize = (Int, Int)
+
 type alias PosElem = {
   pos : (Float, Float)
   , elem : Elem
@@ -16,6 +18,11 @@ type alias PosElem = {
 
 type alias Model = {
   elems : List PosElem
+}
+
+type alias Input = {
+  time: Time
+  , mousePos: MousePos
 }
 
 
@@ -32,13 +39,33 @@ cn = {
   subTitle = "looser",
   imageUrl = "cn.jpg"}
 
+
+initial : Model
+initial = { elems = [
+  { pos = (0, 0), elem = ww }
+  , { pos = (0, 0), elem = cn }]
+  }
+
+
 pos : Time -> Float
 pos t = 400 * (sin (t * 0.002))
 
-view : (Int, Int) -> Time -> MousePos -> Element
-view (w, h) t (x, y) = collage w h [
-  elemForm cn (pos t) (toFloat x),
-  elemForm ww 0 (pos t)]
+view : ScreenSize -> Model -> Element
+view (w, h) model = collage w h [
+  elemForm cn 10 10,
+  elemForm ww -10 -10]
+
+updateElem : Time -> MousePos -> PosElem -> PosElem
+updateElem time mousePos posElem = posElem
+
+
+update : Input -> Model -> Model
+update input model =
+  { model |elems = List.map
+    (updateElem input.time input.mousePos)
+    model.elems
+  }
+
 
 time : Signal Time
 time = Time.every (Time.second * 0.001)
@@ -49,10 +76,19 @@ mousePos =
   Signal.sampleOn Mouse.clicks Mouse.position
 
 
+input : Time
+  -> MousePos
+  -> Input
+input time mousePos = {
+  time = time
+  , mousePos = mousePos}
+
+
 main : Signal Element
-main = Signal.map3
+main = Signal.map2
   view
   Window.dimensions
-  time
-  mousePos
-
+  (Signal.foldp
+    update
+    initial
+    (Signal.map2 input time mousePos))
