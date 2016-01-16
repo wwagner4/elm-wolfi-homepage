b4011030d152a758842605174ae467a7e8b949f4
wwagner4 <wwagner4@gmail.com>
2016-01-16 11:15:59 +0100

    added initial seed

--- tryouts/randomWalk.elm
+++ tryouts/randomWalk.elm
@@ -1,3 +1,5 @@
+import Random exposing (..)
+
 
 type alias PanelDim = {
   w : Float
@@ -17,23 +19,37 @@ type alias Model = {
   elem : Elem
 }
 
-
+seed = initialSeed 0
 
 
 initialPos : Pos
 initialPos = { x = 0.0, y = 0.0 }
 
+
 initialElem : Elem
 initialElem = { pos = initialPos }
+
 
 initial : Model
 initial = { elem = initialElem }
 
 
+ranDiff : Float
+ranDiff = 0.1
+
+
+updateX : PanelDim -> Pos -> Float
+updateX panel pos = pos.x + ranDiff
+
+
+updateY : PanelDim -> Pos -> Float
+updateY panel pos = pos.y + ranDiff
+
+
 updatePos : PanelDim -> Pos -> Pos
 updatePos panel pos = { pos |
-  x = 0.0 ,
-  y = 0.0 }
+  x = updateX panel pos  ,
+  y = updateY panel pos }
 
 updateElem : PanelDim -> Elem -> Elem
 updateElem panel elem = { elem |
